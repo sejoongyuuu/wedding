cd1d08f7e2b091370cb47ab670df8dbd902f0da5
yuuu1231 <choiyujeong1231@gmail.com>
2022-07-28 00:32:33 +0900

    웹폰트 원복

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/global.css'
-/*import '../public/fonts/fonts.css'*/
+import '../public/fonts/fonts.css'
 
 function MyApp(props) {
     const {Component, pageProps} = props;
--- pages/_document.js
+++ pages/_document.js
@@ -7,11 +7,6 @@ export default class MyDocument extends Document {
         return (
             <Html lang="ko">
                 <Head>
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
-                        rel="stylesheet"
-                        type="text/css"
-                    />
                 </Head>
                 <body>
                 <Main/>
