b678e25383a0575128ae027abdacef892a2811da
yuuu1231 <choiyujeong1231@gmail.com>
2022-07-28 01:23:15 +0900

    deploy to gh-pages

---
+++ _next/static/qcCpJfgvhe44d9nZUGPNL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/36-c85bee87f6c03760.js","static/css/f54815db9838b726.css","static/chunks/pages/index-f09b1f2601ec63f0.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
