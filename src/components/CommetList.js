5fbb799684068d8f2fbed018a6e7d2246b8df072
yuuu1231 <choiyujeong1231@gmail.com>
2022-07-28 20:07:47 +0900

    방명록 디자인 수정

--- src/components/CommetList.js
+++ src/components/CommetList.js
@@ -1,40 +1,71 @@
+import * as React from "react";
+import {useState} from "react";
 import {
+    Box,
     Button,
     CircularProgress,
     Dialog,
     DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
+    DialogContent, Paper, Popper,
     TextField
 } from "@mui/material";
-import * as React from "react";
-import {useState} from "react";
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import {red} from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Fade from '@mui/material/Fade';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function CommentList(props) {
     const {loading, comments, getComments} = props;
 
     const [open, setOpen] = useState(false);
     const [password, setPassword] = useState("");
-    const [tobeDelete, setTobeDelete] = useState(null);
+    const [target, setTarget] = useState(null);
     const [invalid, setInvalid] = useState(false);
 
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [popperOpen, setPopperOpen] = React.useState(false);
+    const [placement, setPlacement] = React.useState();
+
+    const handleClick = (newPlacement, comment) => (event) => {
+        console.log("handleClick, comment=>" + comment)
+        setTarget(comment);
+        setAnchorEl(event.currentTarget);
+        setPopperOpen((prev) => placement !== newPlacement || !prev);
+        setPlacement(newPlacement);
+    };
+
     const handleClickOpen = (comment) => {
-        setTobeDelete(comment);
+        console.log("handleClickOpen, comment=>" + comment)
+        setInvalid(false);
+        setPassword("");
+        setPopperOpen(false);
+        setTarget(comment);
         setOpen(true);
     };
 
     const handleClose = () => {
+        setInvalid(false);
+        setPassword("");
         setOpen(false);
+        setTarget(null);
     };
     const handleChange = e => {
         setPassword(e.target.value);
     }
     const handleConfirm = () => {
-        if (tobeDelete.password === password) {
-            deleteComment(tobeDelete._id).then(r =>
+        console.log("handleConfirm => " + target)
+        if (target.password === password) {
+            deleteComment(target._id).then(r =>
                 getComments());
-            setTobeDelete(null);
+            setTarget(null);
             setPassword("");
             setInvalid(false);
             handleClose();
@@ -64,21 +95,53 @@ export default function CommentList(props) {
                     <div>
                         {comments.map(comment => (
                             <div key={comment._id}>
-                                {comment.name} : {comment.content}<br/>
-                                {comment.createdDate}
-                                <Button variant="outlined" onClick={() => handleClickOpen(comment)}>
-                                    delete
-                                </Button>
+                                <Box sx={{
+                                    '& .MuiTextField-root': {m: 1, width: '25ch'},
+                                }}>
+
+                                    <Card sx={{maxWidth: 345}}
+                                          style={{textAlign: 'left', margin: 'auto', marginBottom: '2%'}}>
+                                        <CardHeader
+                                            avatar={
+                                                <Avatar sx={{bgcolor: red[500], fontFamily: 'Noto Sans KR'}}
+                                                        aria-label="recipe">
+                                                    {comment.name.substring(0, 1)}
+                                                </Avatar>
+                                            }
+/*                                            action={
+                                                <IconButton aria-label="settings">
+                                                    <MoreVertIcon/>
+                                                </IconButton>
+
+                                            }*/
+                                            title={comment.name}
+                                            subheader={comment.createdDate.toString()}
+                                        />
+                                        <CardContent>
+                                            <Typography variant="body2" color="text.secondary">
+                                                {comment.content}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions disableSpacing>
+                                            <IconButton aria-label="add to favorites">
+                                                <FavoriteIcon/>
+                                            </IconButton>
+                                            <IconButton onClick={() => handleClickOpen(comment)}>
+                                                <DeleteIcon/>
+                                            </IconButton>
+                                        </CardActions>
+                                    </Card>
+                                </Box>
                             </div>
                         ))}
                     </div>
                     <div>
                         <Dialog open={open} onClose={handleClose}>
-                            <DialogTitle>방명록 삭제</DialogTitle>
+                            <div className="medium">방명록 삭제</div>
                             <DialogContent>
-                                <DialogContentText>
+                                <div>
                                     삭제하시려면 비밀번호를 입력해주세요.
-                                </DialogContentText>
+                                </div>
                                 <TextField
                                     autoFocus
                                     type="password"
