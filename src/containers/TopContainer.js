974b63127cf04f7178e85da6bc81c170a955fa3f
yuuu1231 <choiyujeong1231@gmail.com>
2022-08-01 17:50:49 +0900

    애니메이션 적용

--- src/containers/TopContainer.js
+++ src/containers/TopContainer.js
@@ -7,6 +7,7 @@ export default function TopContainer() {
     const targetImage = useRef(null);
     const targetText = useRef(null);
     const [scrollY, setScrollY] = useState(0);
+    const [animate, setAnimate] = useState(false);
 
     useEffect(() => {
         document.getElementById('html')?.scrollTo(0, 0);
@@ -23,32 +24,40 @@ export default function TopContainer() {
         const y = window.scrollY;
         setScrollY(y);
         console.log(y);
-        transformImage(y);
+        revealImage(y);
 
     });
 
     const transformImage = (scrollY) => {
-        let value = (scrollY <= 200)?  1 + scrollY / 1000 : 1.2;
+        let value = (scrollY <= 200) ? 1 + scrollY / 1000 : 1.2;
         console.log('value: ' + value);
         targetImage.current.style.transform = `scale(${value}, ${value})`;
     }
 
-    const transformText = (scrollY) => {
+    const revealImage = (scrollY) => {
+        let value = (scrollY <= 200) ? scrollY / 200 + 0.3 : 1;
+        if (value > 1) value = 1;
+        targetImage.current.style({opacity: value});
     }
 
+
     return (
-        <div className={styles.container} >
-            <div className={styles.name} ref={targetText}>
-                <div>SEJOONG</div>
-                <div>& YUJEONG</div>
+        <div className={styles.container}>
+            <div className={styles.name}>
+                <div className={styles.TitleMask}>
+                    <div className={styles.Title}>SEJOONG</div>
+                </div>
+                <div className={styles.TitleMask}>
+                    <div className={styles.Title_2}>& YUJEONG</div>
+                </div>
             </div>
-            <div className={styles.thin}>
-                SAVE THE DATE
-            </div>
-            <div ref={targetImage} className={styles.image}>
-                <Image src={picture} alt="" width="867.2" height="788"/>
-            </div>
-            <div>
+            <div className={styles.appear}>
+                <div className={styles.thin}>
+                    SAVE THE DATE
+                </div>
+                <div ref={targetImage} className={styles.image}>
+                    <Image src={picture} alt="" width="867.2" height="788"/>
+                </div>
                 <div className={styles.textContainer}>
                     <div className={styles.date}>
                         <div className={styles.first}>2022</div>
