2aab6e150faba6fc6547131a47039cf9a2030309
yuuu1231 <choiyujeong1231@gmail.com>
2022-08-29 09:30:16 +0900

    ui 수정

--- src/containers/CalendarContainer.js
+++ src/containers/CalendarContainer.js
@@ -3,16 +3,17 @@ import styles from '../../styles/calendar.module.css';
 import Fade from 'react-reveal/Fade';
 import Image from "next/image";
 import leaf from "../../public/images/shape/leaf.png";
+import star from "../../public/images/shape/star.png";
 
 export default function CalendarContainer() {
     return (
         <>
-            <Fade>
+            <Fade bottom>
                 <div>
                     <div className='titleDiv'>The Wedding Day</div>
                     <div className={styles.date}>9월 18일 일요일 오후 3시</div>
                 </div>
-                <div style={{width: '90%', margin:'auto'}}>
+                <div style={{width: '90%', margin: 'auto'}}>
                     <Calendar/>
                 </div>
             </Fade>
